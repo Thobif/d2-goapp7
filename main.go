acd98b2f1754ce0bcd3c11fcf28e9d98fb9b07a0
Thobif <jirayootpandee537@gmail.com>
2021-12-17 15:44:29 +0700

    initial update

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Thobif/d2-goapp7.git/variables"
+)
+
+func main() {
+	// fmt.Println("Hello World");
+	// fmt.Println(quote.HelloV3())
+	variables.Learn()
+	variables.Learn()
+	fmt.Println("End Function")
+}
+
+
